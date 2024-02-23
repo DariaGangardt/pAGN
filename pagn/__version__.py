27df92631eeed082775a7b0586b4d1e89e066563
ddg672 <ddg672@student.bham.ac.uk>
2024-02-23 10:37:31 +0000

    Simplifying setup

--- pagn/__version__.py
+++ pagn/__version__.py
@@ -1,14 +1,6 @@
 """
-Information about the current version of the pAGN package.
+Information about the current version of the pagn package.
 """
 
-__title__ = 'pAGN'
-__description__ = 'Solutions to parametric AGN disk models'
 __version__ = '0.0.12'
-__author__ = 'Daria Gangardt, Alessandro A. Trani'
-__author_email__ = 'ddg672@student.bham.ac.uk, aatrani@gmail.com'
-__license__ = 'GPLv3'
-__url__ = 'https://github.com/DariaGangardt/pAGN'
-
-__all__ = ['__title__', '__description__', '__version__', '__author__',
-           '__author_email__', '__license__', '__url__']
+__all__ = ['__version__']
--- setup.py
+++ setup.py
@@ -6,14 +6,6 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'pagn', '__version__.py'), 'r') as _:
     exec(_.read(), about)
 
-with open ('requirements.txt', 'r') as _:
-    requires = [line.split()[0] for line in _]
-
 setuptools.setup(
-    long_description='See: '+about['__url__'],
-    long_description_content_type='text/markdown',
     version=about['__version__'],
-    url=about['__url__'],
-    packages=setuptools.find_packages(),
-    zip_safe=False
 )
